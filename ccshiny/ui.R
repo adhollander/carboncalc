00623a3f80812d5c32c192f9d9a12ac6f856b02a
Allan Hollander <adhollander@ucdavis.edu>
2016-08-23 14:42:57 -0700

    Include growth results

--- ccshiny/ui.R
+++ ccshiny/ui.R
@@ -13,40 +13,10 @@ shinyUI(fluidPage(
   # Application title
   titlePanel("Urban Forestry Carbon Calculator"),
   verticalLayout(
-    rHandsontableOutput("hot", width=900)),
+    rHandsontableOutput("hot", width=1000)),
     br(),
     downloadButton("download_table", "Download Table"),
     actionButton("calc_results", "Calculate!"),
     br(),
     fileInput("uploadedsheet", "Upload Spreadsheet")
-  ))
-  # Sidebar with a slider input for number of bins
-#   sidebarLayout(
-#     sidebarPanel(
-#       sliderInput("bins",
-#                   "Number of bins:",
-#                   min = 1,
-#                   max = 50,
-#                   value = 30)
-#     ),
-# 
-#     # Show a plot of the generated distribution
-#     mainPanel(
-#       plotOutput("distPlot")
-#     )
-#   )
-#))
-# 
-# shinyUI(fluidPage(
-#   titlePanel("Handsontable"),
-#   sidebarLayout(
-#     sidebarPanel(
-#       helpText("Handsontable demo output. Column add/delete does work ",
-#                "for tables with defined column properties, including type."),
-#       radioButtons("useType", "Use Data Types", c("TRUE", "FALSE"))
-#     ),
-#     mainPanel(
-#       rHandsontableOutput("hot", width = 500)
-#     )
-#   )
-# ))
+))
