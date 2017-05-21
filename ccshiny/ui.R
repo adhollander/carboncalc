b209afd4d27d0f2db74858174efaa05ea7dff78e
adhollander <adhollander@ucdavis.edu>
2017-05-21 16:11:06 -0700

    Added descriptive comment.

--- ccshiny/ui.R
+++ ccshiny/ui.R
@@ -1,9 +1,4 @@
-
-# This is the user-interface definition of a Shiny web application.
-# You can find out more about building applications with Shiny here:
-#
-# http://shiny.rstudio.com
-#
+# Layout code for preliminary interface in R/Shiny for carbon calculator.
 
 library(shiny)
 library(rhandsontable)
